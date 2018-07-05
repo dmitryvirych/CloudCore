9b8693c3547aaafab1b4737bb18c8f94e7bd7b6d
Dmitry Virych <dmitry.virych@requestum.com>
2018-07-05 13:54:41 +0300

    bugs fixed

--- Source/Classes/Transformers/ValuesTransformer.swift
+++ Source/Classes/Transformers/ValuesTransformer.swift
@@ -3,37 +3,37 @@
 //  CloudCore
 //
 //  Created by Dima Virych on 05.07.2018.
-//  Copyright © 2018 Vasily Ulianov. All rights reserved.
+//  Copyright © 2018 Requestum. All rights reserved.
 //
 
 import Foundation
 
 public class ValuesTransformerInterface {
     
-    static func register(_ transformer: ObjectTransformer<NSObject>, for name: String) {
+    public static func register(_ transformer: ValueTransformer, for name: String) {
         
         ValueTransformer.setValueTransformer(transformer, forName: NSValueTransformerName(rawValue: name))
     }
 }
 
-class ObjectTransformer<T: NSObject>: ValueTransformer {
+public class ObjectTransformer<T: NSObject>: ValueTransformer {
     
-    override class func transformedValueClass() -> AnyClass {
+    override public class func transformedValueClass() -> AnyClass {
         return T.self
     }
     
-    override class func allowsReverseTransformation() -> Bool {
+    override public class func allowsReverseTransformation() -> Bool {
         return true
     }
     
-    override func transformedValue(_ value: Any?) -> Any? {
+    override public func transformedValue(_ value: Any?) -> Any? {
         guard let value = value else {
             return nil
         }
         return NSKeyedArchiver.archivedData(withRootObject: value)
     }
     
-    override func reverseTransformedValue(_ value: Any?) -> Any? {
+    override public func reverseTransformedValue(_ value: Any?) -> Any? {
         guard let value = value as? NSData else {
             return nil
         }
