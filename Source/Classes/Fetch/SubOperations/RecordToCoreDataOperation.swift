00609a44d89bf58a59adecbf3770e2ebec3c4f6a
Dmitry Virych <dmitry.virych@requestum.com>
2018-07-12 16:54:35 +0300

    Updated field to binary data

--- Source/Classes/Fetch/SubOperations/RecordToCoreDataOperation.swift
+++ Source/Classes/Fetch/SubOperations/RecordToCoreDataOperation.swift
@@ -11,25 +11,25 @@ import CloudKit
 
 /// Convert CKRecord to NSManagedObject and save it to parent context, thread-safe
 class RecordToCoreDataOperation: AsynchronousOperation {
-	let parentContext: NSManagedObjectContext
-	let record: CKRecord
-	var errorBlock: ErrorBlock?
-	
+    let parentContext: NSManagedObjectContext
+    let record: CKRecord
+    var errorBlock: ErrorBlock?
+    
     /// - Parameters:
     ///   - parentContext: operation will be safely performed in that context, **operation doesn't save that context** you need to do it manually
     ///   - record: record that will be converted to `NSManagedObject`
-	init(parentContext: NSManagedObjectContext, record: CKRecord) {
-		self.parentContext = parentContext
-		self.record = record
-		
-		super.init()
-		
-		self.name = "RecordToCoreDataOperation"
-	}
-	
-	override func main() {
-		if self.isCancelled { return }
-
+    init(parentContext: NSManagedObjectContext, record: CKRecord) {
+        self.parentContext = parentContext
+        self.record = record
+        
+        super.init()
+        
+        self.name = "RecordToCoreDataOperation"
+    }
+    
+    override func main() {
+        if self.isCancelled { return }
+        
         parentContext.perform {
             do {
                 try self.setManagedObject(in: self.parentContext)
@@ -39,61 +39,57 @@ class RecordToCoreDataOperation: AsynchronousOperation {
             
             self.state = .finished
         }
-	}
-	
-	/// Create or update existing NSManagedObject from CKRecord
-	///
-	/// - Parameter context: child context to perform fetch operations
-	private func setManagedObject(in context: NSManagedObjectContext) throws {
-		let entityName = record.recordType
-		
-		guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: context) else {
-			throw CloudCoreError.coreData("Unable to find entity specified in CKRecord: " + entityName)
-		}
-		guard let serviceAttributes = NSEntityDescription.entity(forEntityName: entityName, in: context)?.serviceAttributeNames else {
-			throw CloudCoreError.missingServiceAttributes(entityName: entityName)
-		}
-		
-		// Try to find existing objects
-		let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
-		fetchRequest.predicate = NSPredicate(format: serviceAttributes.recordID + " == %@", record.recordID.encodedString)
-		
-		if let foundObject = try context.fetch(fetchRequest).first as? NSManagedObject {
-			try fill(object: foundObject, entityName: entityName, serviceAttributeNames: serviceAttributes, context: context)
-		} else {
-			let newObject = NSManagedObject(entity: entity, insertInto: context)
-			try fill(object: newObject, entityName: entityName, serviceAttributeNames: serviceAttributes, context: context)
-		}
-	}
-	
-	
-	/// Fill provided `NSManagedObject` with data
-	///
-	/// - Parameters:
-	///   - entityName: entity name of `object`
-	///   - recordDataAttributeName: attribute name containing recordData
-	private func fill(object: NSManagedObject, entityName: String, serviceAttributeNames: ServiceAttributeNames, context: NSManagedObjectContext) throws {
-		for key in record.allKeys() {
-			let recordValue = record.value(forKey: key)
-            
+    }
+    
+    /// Create or update existing NSManagedObject from CKRecord
+    ///
+    /// - Parameter context: child context to perform fetch operations
+    private func setManagedObject(in context: NSManagedObjectContext) throws {
+        let entityName = record.recordType
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: context) else {
+            throw CloudCoreError.coreData("Unable to find entity specified in CKRecord: " + entityName)
+        }
+        guard let serviceAttributes = NSEntityDescription.entity(forEntityName: entityName, in: context)?.serviceAttributeNames else {
+            throw CloudCoreError.missingServiceAttributes(entityName: entityName)
+        }
+        
+        // Try to find existing objects
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        fetchRequest.predicate = NSPredicate(format: serviceAttributes.recordID + " == %@", record.recordID.encodedString)
+        
+        if let foundObject = try context.fetch(fetchRequest).first as? NSManagedObject {
+            try fill(object: foundObject, entityName: entityName, serviceAttributeNames: serviceAttributes, context: context)
+        } else {
+            let newObject = NSManagedObject(entity: entity, insertInto: context)
+            try fill(object: newObject, entityName: entityName, serviceAttributeNames: serviceAttributes, context: context)
+        }
+    }
+    
+    
+    /// Fill provided `NSManagedObject` with data
+    ///
+    /// - Parameters:
+    ///   - entityName: entity name of `object`
+    ///   - recordDataAttributeName: attribute name containing recordData
+    private func fill(object: NSManagedObject, entityName: String, serviceAttributeNames: ServiceAttributeNames, context: NSManagedObjectContext) throws {
+        
+        for key in record.allKeys() {
+            let recordValue = record.value(forKey: key)
             let attribute = CloudKitAttribute(value: recordValue, fieldName: key, entityName: entityName, serviceAttributes: serviceAttributeNames, context: context)
             let coreDataValue = try attribute.makeCoreDataValue()
-            if let attribute = object.entity.attributesByName[key], attribute.attributeType == .transformableAttributeType {
-                if let name = attribute.valueTransformerName, let transformer = ValueTransformer(forName: NSValueTransformerName(rawValue: name)) {
-                    let value = transformer.transformedValue(coreDataValue)
-                    object.setValue(value, forKey: key)
-                } else if let data = coreDataValue as? Data, let unarchivedObject = NSKeyedUnarchiver.unarchiveObject(with: data) {
-                    object.setValue(unarchivedObject, forKey: key)
-                } else {
-                    object.setValue(coreDataValue, forKey: key)
-                }
+            
+            if let value = coreDataValue as? Data, let obj = NSKeyedUnarchiver.unarchiveObject(with: value) {
+                
+                object.setValue(obj, forKey: key)
             } else {
                 object.setValue(coreDataValue, forKey: key)
             }
         }
-		
-		// Set system headers
-		object.setValue(record.recordID.encodedString, forKey: serviceAttributeNames.recordID)
-		object.setValue(record.encdodedSystemFields, forKey: serviceAttributeNames.recordData)
-	}
+        
+        
+        // Set system headers
+        object.setValue(record.recordID.encodedString, forKey: serviceAttributeNames.recordID)
+        object.setValue(record.encdodedSystemFields, forKey: serviceAttributeNames.recordData)
+    }
 }
